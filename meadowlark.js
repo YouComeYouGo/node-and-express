aaf3a5b2745f0d0009d7d5fe8f3a80764c30b2ad
YouComeYouGo <caiyongbin@139.com>
2015-01-26 00:19:11 +0800

    add tests

--- meadowlark.js
+++ meadowlark.js
@@ -14,6 +14,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res){
     //res.type('text/plain');
     //res.send('Meadowlark Travel');
@@ -25,7 +30,10 @@ app.get('/about', function(req, res){
     //res.send('About Meadowlark Travel');
     //var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
     //res.render('about', { fortune: randomFortune });
-    res.render('about', { fortune: fortune.getFortune() } );
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    } );
 });
 
 // 定制404页面
@@ -47,5 +55,5 @@ app.use(function(err, req, res, next){
 
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
+        app.get('port') + '; press Ctrl-C to terminate. ');
 });
